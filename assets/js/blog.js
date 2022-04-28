edd96e091a0fb6e88128d03facba091805a86f9d
Vinit Parekh <vinitrevaliya@gmail.com>
2022-04-29 01:05:34 +0530

    blog included

---
+++ assets/js/blog.js
@@ -0,0 +1,47 @@
+"use strict"
+let container = document.querySelector('[data-container]')
+let tamplate = document.querySelector('[data-tamplate]')
+
+const GET_USER_ARTICLES = `
+    query GetUserArticles($page: Int!) {
+        user(username: "vinit17") {
+            publication {
+                posts(page: $page) {
+                    title
+                    brief
+                    slug
+                }
+            }
+        }
+    }
+`;
+async function gql(query, variables = {}) {
+    const data = await fetch('https://api.hashnode.com/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query,
+            variables
+        })
+    });
+    return data;
+}
+
+gql(GET_USER_ARTICLES, { page: 0 })
+    .then(result => result.json())
+    .then(data => {
+        let articles = data.data.user.publication.posts
+        articles.map(a => {
+            const card = tamplate.content.cloneNode(true).children[0]
+            const title = card.querySelector('[data-title]').childNodes[1]
+            const description = card.querySelector('[data-description]')
+            title.textContent = a.title 
+            title.setAttribute('href', `https://blog.vinitparekh.rocks/${a.slug}`)
+            title.setAttribute('target', '_blank')
+            description.textContent = a.brief.slice(0,100) + ' ...'
+            container.append(card)
+        })
+        console.log(articles);
+    })
